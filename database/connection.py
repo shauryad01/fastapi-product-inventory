3472401622065ea104258d3b87c659ac21dbde65
Shaurya Dhingra <shauryadhingra1@gmail.com>
2026-02-26 14:06:29 +0530

    Shifted database url to env

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 
-DB_URL="postgresql://postgres:user@localhost:5432/demo"
-engine = create_engine(DB_URL)
+load_dotenv()
+
+db_url=os.getenv('DB_URL')
+engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
